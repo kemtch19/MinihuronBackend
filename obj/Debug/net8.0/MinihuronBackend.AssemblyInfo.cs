9b742d1b961e70343b75f880f35da990689e4559
kemtch19 <kemtch19@gmail.com>
2024-07-03 19:05:17 -0500

    delete: se borra contexto repetido de la carpeta data

--- obj/Debug/net8.0/MinihuronBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/MinihuronBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinihuronBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e9ebe565cf9ccb7a6cfd97735970a8f86b6ed5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+228a633c45beba0ff954bb34173169165768d540")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinihuronBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinihuronBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
